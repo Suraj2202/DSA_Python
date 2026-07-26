e958babce03e7bd1b6187114084386fa78b4aec6
suraj <suraj@DESKTOP-AL8O0JP>
2026-07-26 22:26:12 +0530

    only hard left sliding window

--- sliding_window/variable/LeetCode/438 Find All Anagrams in a String.py
+++ sliding_window/variable/LeetCode/438 Find All Anagrams in a String.py
@@ -12,19 +12,54 @@ and a Variable window approach using character frequency matching.
 """
 
 # region Inputs
-s1, p1 = "cbaebabacd", "abc"   # Expected: [0, 6]
-s2, p2 = "abab",       "ab"    # Expected: [0, 1, 2]
-s3, p3 = "af",         "be"    # Expected: []
+s1, p1 = "cbaebabacd", "abc"  # Expected: [0, 6]
+s2, p2 = "abab", "ab"  # Expected: [0, 1, 2]
+s3, p3 = "af", "be"  # Expected: []
 # endregion
 
 
 # region Methods
 def brute_force_find_all_anagrams(s, p):
-    pass
+    res = []
+    pFreq = {}
+    for x in p:
+        pFreq[x] = pFreq.get(x, 0) + 1
+
+    for i in range(len(s) - len(p) + 1):
+        check = {}
+        for j in range(i, i + len(p)):
+            check[s[j]] = check.get(s[j], 0) + 1
+        if check == pFreq:
+            res.append(i)
+    return res
 
 
 def variable_sliding_window_find_all_anagrams(s, p):
-    pass
+    pFreq = {}
+
+    for x in p:
+        pFreq[x] = pFreq.get(x, 0) + 1
+
+    window = {}
+    res = []
+    for i in range(len(p)):
+        window[s[i]] = window.get(s[i], 0) + 1
+
+    if pFreq == window:
+        res.append(0)
+
+    for i in range(len(p), len(s)):
+        window[s[i]] = window.get(s[i], 0) + 1
+
+        window[s[i - len(p)]] -= 1
+        if window[s[i - len(p)]] == 0:
+            window.pop(s[i - len(p)])
+
+        if window == pFreq:
+            res.append(i - len(p) + 1)
+    return res
+
+
 # endregion
 
 
--- sliding_window/variable/LeetCode/567 Permutation in String.py
+++ sliding_window/variable/LeetCode/567 Permutation in String.py
@@ -12,19 +12,66 @@ and a Variable window approach using character frequency matching.
 """
 
 # region Inputs
-s1_1, s2_1 = "ab",  "eidbaooo"   # Expected: True
-s1_2, s2_2 = "ab",  "eidboaoo"   # Expected: False
-s1_3, s2_3 = "adc", "dcda"       # Expected: True
+s1_1, s2_1 = "ab", "eidbaooo"  # Expected: True
+s1_2, s2_2 = "ab", "eidboaoo"  # Expected: False
+s1_3, s2_3 = "adc", "dcda"  # Expected: True
 # endregion
 
 
 # region Methods
 def brute_force_permutation_in_string(s1, s2):
-    pass
-
+    s1Freq = {}
+    for x in s1:
+        s1Freq[x] = s1Freq.get(x, 0) + 1
+    for i in range(len(s2)):
+        r = i
+        checkFreq = {}
+        while r < len(s2) and (r - i) < len(s1):
+            checkFreq[s2[r]] = checkFreq.get(s2[r], 0) + 1
+            if checkFreq == s1Freq:
+                return True
+            r += 1
+    return False
 
 def variable_sliding_window_permutation_in_string(s1, s2):
-    pass
+    if len(s1) > len(s2):
+        return False
+
+    need = {}
+    for ch in s1:
+        need[ch] = need.get(ch, 0) + 1
+
+    window = {}
+    left = 0
+
+    for right in range(len(s2)):
+        ch = s2[right]
+
+        # Character not present in s1 -> reset window
+        if ch not in need:
+            window.clear()
+            left = right + 1
+            continue
+
+        # Add current character
+        window[ch] = window.get(ch, 0) + 1
+
+        # Shrink until current character frequency is valid
+        while window[ch] > need[ch]:
+            left_char = s2[left]
+            window[left_char] -= 1
+
+            if window[left_char] == 0:
+                del window[left_char]
+
+            left += 1
+
+        # If frequencies match, permutation found
+        if window == need:
+            return True
+
+    return False
+
 # endregion
 
 
